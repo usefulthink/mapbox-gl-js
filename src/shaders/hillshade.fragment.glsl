3a2eaf88d5693b3bbb12bcd04351276b64a88969
Molly Lloyd <molly@mapbox.com>
2017-09-01 15:40:51 -0700

    fix shader

--- src/shaders/hillshade.fragment.glsl
+++ src/shaders/hillshade.fragment.glsl
@@ -1,3 +1,4 @@
+
 #ifdef GL_ES
 precision mediump float;
 #else
@@ -8,41 +9,69 @@ precision mediump float;
 
 uniform sampler2D u_image;
 varying vec2 v_pos;
-uniform vec2 u_dimension;
-uniform float u_zoom;
 
-float getElevation(vec2 coord, float bias) {
-    // Convert encoded elevation value to meters
-    vec4 data = texture2D(u_image, coord, bias) * 255.0;
-    // return data.r * 256.0 + data.g + data.b / 256.0 - 32768.0;
-    // return ((data.r * 256.0 * 256.0 + data.g * 256.0 + data.b) / 10.0 - 10000.0) * 2.0;
-    return (data.r + data.g * 256.0 + data.b * 256.0 * 256.0) / 4.0;
-}
+uniform int u_mode;
+uniform float u_mipmap;
+uniform float u_lightintensity;
+uniform vec3 u_lightpos;
+
+uniform vec4 u_shadow;
+uniform vec4 u_highlight;
+uniform vec4 u_accent;
+
+#define PI 3.141592653589793
+
+const int mode_raw = 0;
+const int mode_elevation = 1;
+const int mode_dxdy = 2;
+const int mode_slope = 3;
+const int mode_aspect = 4;
+const int mode_openness = 5;
+const int mode_hillshade = 6;
+const int mode_color = 7;
+const int mode_coloropen = 8;
 
 void main() {
-    vec2 epsilon = 1.0 / u_dimension;
-    float a = getElevation(v_pos + vec2(-epsilon.x, -epsilon.y), 0.0);
-    float b = getElevation(v_pos + vec2(0, -epsilon.y), 0.0);
-    float c = getElevation(v_pos + vec2(epsilon.x, -epsilon.y), 0.0);
-    float d = getElevation(v_pos + vec2(-epsilon.x, 0), 0.0);
-    float e = getElevation(v_pos, 0.0);
-    float f = getElevation(v_pos + vec2(epsilon.x, 0), 0.0);
-    float g = getElevation(v_pos + vec2(-epsilon.x, epsilon.y), 0.0);
-    float h = getElevation(v_pos + vec2(0, epsilon.y), 0.0);
-    float i = getElevation(v_pos + vec2(epsilon.x, epsilon.y), 0.0);
+    vec4 pixel = texture2D(u_image, v_pos, u_mipmap);
 
-    vec2 deriv = vec2(
-        (c + f + f + i) - (a + d + d + g),
-        (g + h + h + i) - (a + b + b + c)
-        // decrease the value u_zoom is subtracted from to intensify terrain appearance
-    ) / pow(2.0, 19.0 - u_zoom);
+    vec2 deriv = ((pixel.rg * 2.0) - 1.0);
 
+    float slope = atan(1.25 * length(deriv));
+    float aspect = deriv.x != 0.0 ? atan(deriv.y, -deriv.x) : PI / 2.0 * (deriv.y > 0.0 ? 1.0 : -1.0);
+    float openness = pixel.b;
 
-    gl_FragColor = clamp(vec4(
-        deriv.x / 2.0 + 0.5,
-        deriv.y / 2.0 + 0.5,
-        1.0,
-        1.0), 0.0, 1.0);
+    float azimuth =  u_lightpos.y;
+    float polar = u_lightpos.z;
+
+    if (u_mode == mode_raw) {
+        gl_FragColor = texture2D(u_image, v_pos, u_mipmap);
+    } else if (u_mode == mode_dxdy) {
+        gl_FragColor = clamp(vec4(deriv.x + 0.5, deriv.y + 0.5, deriv.x + 0.5, 1.0), 0.0, 1.0);
+    } else if (u_mode == mode_slope) {
+        gl_FragColor = vec4(1.0 - slope, 1.0 - slope, 1.0 - slope, 1.0);
+    } else if (u_mode == mode_aspect) {
+        float asp = abs(aspect) / PI;
+        gl_FragColor = vec4(asp, asp, asp, 1.0);
+    } else if (u_mode == mode_openness) {
+        gl_FragColor = vec4(openness, openness, openness, 1.0);
+    } else if (u_mode == mode_hillshade) {
+        float hillshade = cos(polar) * cos(slope) + sin(polar) * sin(slope) * cos(azimuth - aspect);
+        gl_FragColor = vec4(hillshade, hillshade, hillshade, 1.0);
+    } else if (u_mode == mode_color) {
+        float accent = cos(slope);
+        vec4 accent_color = u_lightintensity * clamp((1.0 - accent) * 2.0, 0.0, 1.0) * u_accent;
+        float shade = abs(mod((aspect + azimuth) / PI + 0.5, 2.0) - 1.0);
+        vec4 shade_color = mix(u_shadow, u_highlight, shade) * (slope) * sin(polar);
+        gl_FragColor = accent_color * (1.0 - shade_color.a) + shade_color;
+    } else if (u_mode == mode_coloropen) {
+        float accent = cos((1.0 - openness) * 2.0);
+        vec4 accent_color = u_lightintensity * (1.0 - accent) * u_accent;
+        float shade = abs(mod((aspect + azimuth) / PI + 0.5, 2.0) - 1.0);
+        vec4 shade_color = u_lightintensity * slope * sin(polar) * mix(u_shadow, u_highlight, shade);
+        gl_FragColor = accent_color * (1.0 - shade_color.a) + shade_color;
+    } else {
+        gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    }
 
 #ifdef OVERDRAW_INSPECTOR
     gl_FragColor = vec4(1.0);
